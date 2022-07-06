47bd2253ce51ff37cc3ca6be2c2b77f111e645c1
osidekyle <hawkinskyle67@gmail.com>
2022-07-05 22:49:29 -0700

    additional preprocessing

--- medical_imaging_example.py
+++ medical_imaging_example.py
@@ -8,57 +8,85 @@ import math
 
 data_dir=''
 patients = os.listdir(data_dir)
-labels_df = pd.read_csv("stage1_labels.csv", index_col=0)
+labels = pd.read_csv("stage1_labels.csv", index_col=0)
 
 
 labels_df.head()
 
 IMG_PX_SIZE = 150
-
+SLICE_COUNT = 20
 
 
 def chunks(l, n):
     for i in range(0, len(l), n):
         yield l[i:i+n]
 
-for patient in patients[:1]:
-    label = labels_df.get_value(patient, 'cancer')
-    path = data_dir + patient
 
-    slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
-    slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
-    new_slices =[]
+def mean(a):
+    return sum(a) / len(a)
 
-    slices = [cv2.resize(np.array(each_slice.pixel_array), (IMG_PX_SIZE, IMG_PX_SIZE)) for each_slice in slices]
 
-    chunk_sizes = math.ceil(len(slices) / HM_SLICES)
+def process_data(patient, labels_df, img_px_size=50, hm_slices=20, visualization=False):
 
-    for slice_chunk in chunks(slices, chunk_sizes):
-        slice_chunk = list(map(mean, zip(*slice_chunk)))
-        new_slices.append(slice_chunk)
+        label = labels_df.get_value(patient, 'cancer')
+        path = data_dir + patient
 
-    if len(new_slices) == HM_SLICES-1:
-        new_slices.append(new_slices[-1])
+        slices = [dicom.read_file(path + '/' + s) for s in os.listdir(path)]
+        slices.sort(key = lambda x: int(x.ImagePositionPatient[2]))
+        new_slices =[]
 
-    if len(new_slices) == HM_SLICES-2:
-        new_slices.append(new_slices[-1])
-        new_slices.append(new_slices[-1])
+        slices = [cv2.resize(np.array(each_slice.pixel_array), (img_px_size, img_px_size)) for each_slice in slices]
 
-    if len(new_slices) == HM_SLICES + 2:
-        new_val = list(map(mean, zip[new_slices[HM_SLICES=1], new_slices[HM_SLICES],]))
-        del new_slices[HM_SLICES]
-        new_slices[HM_SLICES-1] = new_val
+        chunk_sizes = math.ceil(len(slices) / hm_slices)
 
-    if len(new_slices) == HM_SLICES + 1:
-        new_val = list(map(mean, zip[new_slices[HM_SLICES=1], new_slices[HM_SLICES],]))
-        del new_slices[HM_SLICES]
-        new_slices[HM_SLICES-1] = new_val
+        for slice_chunk in chunks(slices, chunk_sizes):
+            slice_chunk = list(map(mean, zip(*slice_chunk)))
+            new_slices.append(slice_chunk)
 
-    fig = plt.figure()
-    for num,each_slice in enumerate(new_slices):
-        y = fig.add_subplot(4,5,num+1)
-        y.imshow(each_slice, cmap='gray')
-    plt.show()
+        if len(new_slices) == hm_slices-1:
+            new_slices.append(new_slices[-1])
+
+        if len(new_slices) == hm_slices-2:
+            new_slices.append(new_slices[-1])
+            new_slices.append(new_slices[-1])
+
+        if len(new_slices) == hm_slices + 2:
+            new_val = list(map(mean, zip(*[new_slices[hm_slices-1], new_slices[hm_slices],])))
+            del new_slices[hm_slices]
+            new_slices[hm_slices-1] = new_val
+
+        if len(new_slices) == hm_slices + 1:
+            new_val = list(map(mean, zip(*[new_slices[hm_slices-1], new_slices[hm_slices],])))
+            del new_slices[hm_slices]
+            new_slices[hm_slices-1] = new_val
+
+
+        if visualization:
+            fig = plt.figure()
+            for num,each_slice in enumerate(new_slices):
+                y = fig.add_subplot(4,5,num+1)
+                y.imshow(each_slice, cmap='gray')
+            plt.show()
+
+        if label == 1: label=np.array([0,1])
+        elif label == 0: label=np.array([1,0])
+
+        return np.array(new_slices), label
+
+
+much_data = []
+for num,patient in enumerate(patients):
+    if num % 100 == 0:
+        print(num)
+    try:
+        img_data, label = process_data(patient, labels, img_px_size=IMG_PX_SIZE, hm_slices=SLICE_COUNT)
+        much_data.append([img_data, label])
+    except KeyError as e:
+        print("This is unlabeled data")
+
+np.save('muchdata-{}-{}-{}.npy'.format(IMG_PX_SIZE, IMG_PX_SIZE, SLICE_COUNT), much_data)
+
+
 
 
 
